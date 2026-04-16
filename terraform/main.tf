2fe6d2c2b5a9302a10ae24e6d51fecbb0ceec4b3
takatoseki0107 <223703344+takatoseki0107@users.noreply.github.com>
2026-04-17 08:18:39 +0900

    Add Terraform DynamoDB setup

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "terraform"
+}
+resource "aws_dynamodb_table" "household" {
+  name         = "household-transactions"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+  range_key    = "transactionId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "transactionId"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "household-transactions"
+    Environment = "dev"
+  }
+}
