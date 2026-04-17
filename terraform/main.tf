5e1397d286eb20126767dd624247cb55a50515fb
takatoseki0107 <223703344+takatoseki0107@users.noreply.github.com>
2026-04-17 09:53:34 +0900

    Add Cognito user pool and variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region  = "ap-northeast-1"
-  profile = "terraform"
+  profile = var.aws_profile
 }
 
 resource "aws_dynamodb_table" "household" {
@@ -31,6 +31,35 @@ resource "aws_dynamodb_table" "household" {
 
   tags = {
     Name        = "household-transactions"
-    Environment = "dev"
+    Environment = var.environment
   }
+}
+
+resource "aws_cognito_user_pool" "household" {
+  name = "household-user-pool"
+
+  password_policy {
+    minimum_length    = 8
+    require_uppercase = true
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = false
+  }
+
+  auto_verified_attributes = ["email"]
+
+  tags = {
+    Name        = "household-user-pool"
+    Environment = var.environment
+  }
+}
+
+resource "aws_cognito_user_pool_client" "household" {
+  name         = "household-app-client"
+  user_pool_id = aws_cognito_user_pool.household.id
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+  ]
 }
